16401181700cff1771672cdc4d7e62ea02c362b4
Dukul Howlader Mohin <dukul.howlader30@gmail.com>
2021-03-18 22:34:05 +0600

    assignment setup

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBXZFaRVk3UEK8cdXACWdBmiL7OgARJ2ew",
+    authDomain: "metro-riders.firebaseapp.com",
+    projectId: "metro-riders",
+    storageBucket: "metro-riders.appspot.com",
+    messagingSenderId: "799829323640",
+    appId: "1:799829323640:web:4320043ab1d7981d21e28d"
+  };
+  export default firebaseConfig;
